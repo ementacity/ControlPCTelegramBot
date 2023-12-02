2efb10192b4e6a825edf33cf9998558cc4bf0f86
Iłya K <97191872+wetakor@users.noreply.github.com>
2023-12-02 23:04:11 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,4 @@
 TOKEN = 'YOUR_BOT_TOKEN' # @BotFather токен бота
 AUTHORIZED_ID = 'TELEGRAM_ID'  # Ваш id в Telegram
+# Процессы которые не выводятся в активности процессов
+IGNORED_PROCESSES = {'taskhostw.exe', 'TrustedInstaller.exe', 'sppsvc.exe', 'TiWorker.exe', 'audiodg.exe', 'fsnotifier.exe', 'QtWebEngineProcess.exe', 'RuntimeBroker.exe', 'backgroundTaskHost.exe', 'ApplicationFrameHost.exe', 'SystemSettings.exe', 'UserOOBEBroker.exe', 'amdow.exe', 'AMDRSServ.exe', 'cncmd.exe', 'clinfo.exe', 'RadeonSoftware.exe','TextInputHost.exe', 'AMDRSSrcExt.exe', 'powershell.exe', 'cmd.exe', 'WmiPrvSE.exe', '', 'dllhost.exe', 'CompPkgSrv.exe', 'SearchProtocolHost.exe', 'SearchFilterHost.exe', 'msedge.exe', 'python.exe', 'pingsender.exe', 'conhost.exe', 'svchost.exe'}
